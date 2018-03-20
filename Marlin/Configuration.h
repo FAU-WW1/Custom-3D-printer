91c645096763ce19eae8791bc5d402a5d509da88
Yvonne25 <yvonne.thompson@fau.de>
2018-03-20 13:16:47 +0100

    PID heatbed
    PID initialization & updated autotuning values

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -388,7 +388,7 @@
 // If your configuration is significantly different than this and you don't understand the issues involved, you probably
 // shouldn't use bed PID until someone else verifies your hardware works.
 // If this is enabled, find your own PID constants below.
-//#define PIDTEMPBED
+#define PIDTEMPBED
 
 //#define BED_LIMIT_SWITCHING
 
@@ -404,9 +404,9 @@
 
 //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
 //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-#define  DEFAULT_bedKp 10.00
-#define  DEFAULT_bedKi .023
-#define  DEFAULT_bedKd 305.4
+#define  DEFAULT_bedKp 164.24
+#define  DEFAULT_bedKi 17.14
+#define  DEFAULT_bedKd 393.56
 
 //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
 //from pidautotune
